a2ed6c5ca5900430313ac45f2491d75335579091
Alina <aakaracharova@mai.education>
2024-04-27 12:46:38 +0300

    sale.html was added and changes were made to home.html

--- pizza/homepage/views.py
+++ pizza/homepage/views.py
@@ -5,7 +5,7 @@ import products.models
 
 
 class HomepageView(django.views.generic.TemplateView):
-    template_name = "menu/menu.html"
+    template_name = "sale/sale.html"
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
