ef007488c87e812327d637857ac7c1d4cabb556e
Alina <aakaracharova@mai.education>
2024-05-08 11:53:33 +0300

    created a cart

--- pizza/homepage/views.py
+++ pizza/homepage/views.py
@@ -5,7 +5,7 @@ import products.models
 
 
 class HomepageView(django.views.generic.TemplateView):
-    template_name = "menu/menu2.html"
+    template_name = "bin/bin.html"
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
